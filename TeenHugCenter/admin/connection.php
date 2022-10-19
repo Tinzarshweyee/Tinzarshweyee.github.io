a512f1f2ccca899fc0f20d562115e7cff0de2fdf
Tin Zar Shwe Yee <shweyeetinzar@gmail.com>
2022-10-19 20:20:13 +0630

    ddd

--- TeenHugCenter/admin/connection.php
+++
@@ -1,8 +0,0 @@
-<?php 
-    try {
-        $db=new PDO("mysql:host=localhost; dbname=testing_db", "root", "");
-        $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    } catch(PDOException $e) {
-        echo $e->getMessage();
-    }
-?>
--- TeenHugCenter/admin/itemdelete.php
+++
@@ -1,9 +0,0 @@
-<?php 
-    include 'connection.php';
-    $did=$_GET['did'];
-
-    $sql=$db->prepare("DELETE FROM item_tbl WHERE itemid='$did'");
-    $sql->execute();
-
-    header('location: item.php');
-?>
